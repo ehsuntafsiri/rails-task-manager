756c122af325aa8e5be0c04c047d6b2f989f976c
Ehsun Tafsiri <ehsuntafsiri@gmail.com>
2017-11-07 15:26:57 +0000

    DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #1. List tasks
+  get "tasks", to: "tasks#index", as: "tasks"
+
+  #3. Add new task
+  get "tasks/new", to: "tasks#new", as: "new_task"
+  post "tasks", to: "tasks#create"
+
+   #2. View details of task
+  get "tasks/:id", to: "tasks#show", as: "task"
+
+
+ #4. Edit a task (mark as completed / update title & details)
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "tasks/:id", to: "tasks#update"
+
+  #5. Remove task
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171107113807) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
